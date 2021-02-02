560ca924914897e19a9dc10baf79a1452a4bf2df
Ednei Vieira <ednei.gois167@gmail.com>
2021-02-02 00:10:11 -0400

    ajuste

---
+++ js/fno.js
@@ -0,0 +1,8 @@
+var button = document.querySelector('[data-js="incluir"]')
+var valorfinanc = document.querySelector('[data-js="valorfinanc"]')
+var numeroDeParc = document.querySelector('[data-js="totalparc"]')
+va
+
+button.onclick = function(){
+    for(var i=0; i < numeroDeParc.nodeValue; )
+}
