9c537e93c972a2cdc09c3f3e4c8f5b418e84e6c2
Ednei Vieira <ednei.gois167@gmail.com>
2021-02-02 00:22:26 -0400

    js

--- js/fno.js
+++ js/fno.js
@@ -1,8 +1,19 @@
 var button = document.querySelector('[data-js="incluir"]')
 var valorfinanc = document.querySelector('[data-js="valorfinanc"]')
 var numeroDeParc = document.querySelector('[data-js="totalparc"]')
-va
 
-button.onclick = function(){
-    for(var i=0; i < numeroDeParc.nodeValue; )
+
+button.onclick=  function(){
+    for(var i=0; i < parseInt(numeroDeParc.value);i++ ){
+        criarpar()
+    }
 }
+ function criarpar(){
+    var divNova = document.createElement("div");
+    var conteudoNovo = document.createTextNode("Olá, cumprimentos!");
+    divNova.appendChild(conteudoNovo); //adiciona o nó de texto à nova div criada
+  
+    // adiciona o novo elemento criado e seu conteúdo ao DOM
+    var divAtual = document.getElementById("div1");
+    document.body.insertBefore(divNova, divAtual);
+ }
