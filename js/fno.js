4ad7ca1063eee11b1a316c5f7546f504982760f4
Ednei Vieira <ednei.gois167@gmail.com>
2021-02-02 21:08:43 -0400

    ajuste

--- js/fno.js
+++ js/fno.js
@@ -8,8 +8,8 @@ var i = 1
 
 button.onclick=  function(){
     for(var i=0; i < parseInt(numeroDeParc.value);i++ ){
-        criarpar()
-      //  criarpsar()
+      //  criarpar()
+        criarpsar()
         
     }
 }
@@ -28,17 +28,18 @@ button.onclick=  function(){
     i++
  }
  function criarpsar(){
-     var $fragment = document.createDocumentFragment();
-     var $tr = document.createElement('tr');
-
+    var $fragment = document.createDocumentFragment();
+    var $tr = document.createElement('tr');
     var trNova = document.createElement("td");
     var contParcela = document.createTextNode(i);
-    var trAtual = document.getElementById("div1");
+    //var trAtual = document.getElementById("div1");
     trNova.appendChild(contParcela);    
     document.body.insertBefore(trNova, trAtual);
 
 
-    //$tr.appendChild(trNova)
+    $tr.appendChild(trNova)
+
+    return $fragment.appendChild($tr);
     i++
 
  }
