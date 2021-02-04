3e00adfebe4fb443c1de5b52646d388c1b0cc347
Ednei Vieira <ednei.gois167@gmail.com>
2021-02-04 00:10:28 -0400

    erro

--- js/fno.js
+++ js/fno.js
@@ -1,16 +1,17 @@
 var button = document.querySelector('[data-js="incluir"]')
 var valorfinanc = document.querySelector('[data-js="valorfinanc"]')
 var numeroDeParc = document.querySelector('[data-js="totalparc"]')
+var taxa = document.querySelector('[data-js="taxa"]')
 var parcela = parseInt(numeroDeParc.value)
 var valor =parseInt(valorfinanc.value)
-//var valorparc = parseInt(valorfinanc.value) /parseInt(numeroDeParc.value)
+var valorparc = parseInt(valorfinanc.value) /parseInt(numeroDeParc.value)
 var i = 1
 //var $tr = document.querySelector('[data-js="tr"]')
 
 button.onclick=  function(){
     for(var i=0; i < parseInt(numeroDeParc.value);i++ ){
         criarpar()
-      //  criarpsar() 
+       criarpsar() 
         
     }
 }
@@ -22,62 +23,35 @@ button.onclick=  function(){
     var contParcela = document.createTextNode(`parcela ${i}`);
 
     var trNova1 = document.createElement("td");//node
-    var contParcela1 = document.createTextNode(`  valor  ${parseInt(valorfinanc.value) /parseInt(numeroDeParc.value)}`);
+    var contParcela1 = document.createTextNode(`valor  ${parseInt(valorfinanc.value) /parseInt(numeroDeParc.value)}`);
 
-   // var contParcela = document.createTextNode(i+i+i+i);
-    //var contParcela = document.createTextNode(i);
+    var trNova2 = document.createElement("td");//node
+    var contParcela2 = document.createTextNode(`  valor  ${(parseInt(valorfinanc.value) * parseInt(numeroDeParc.value))}`);
+
+
 
 
     var trAtual = document.getElementById("div1");
-    var trAtual = document.getElementById("div21");
+   // var trAtual = document.getElementById("div21");
     
     //document.body.insertBefore(trNova, trAtual);
 
     trNova.appendChild(contParcela);
     trNova1.appendChild(contParcela1);
+    trNova2.appendChild(contParcela2);
+
     $tr.appendChild(trNova)
     $tr.appendChild(trNova1)
+    $tr.appendChild(trNova2)
 
 
     document.body.insertBefore($tr, trAtual);
 
 
     i++
+    valorfinanc--
     //parcela--
  }
  function criarpsar(){
-    return {
-        init: function(){
-          console.log('inicio init');// apenas para não me perder
-          this.companyInfo();// this neste caso esta referenciando ele mesmo. como se fosse "app.companyInfo()"
-          this.initEvents();
-        },
   
-        initEvents: function initEvents(){
-          new DOM('[data-js="form-register"]').on('submit', this.handleSubmit);
-        },
-  
-        handleSubmit: function handleSubmit(e){
-          e.preventDefault();
-          console.log('submit');
-          var $tableCar = $('[data-js="fno"]').get();
-          $tableCar.appendChild(app.createNewCar());    
-        },
-
-        createNewCar: function createNewCar(){
-        var $fragment = document.createDocumentFragment();
-        var $tr = document.createElement('tr');
-        var trNova = document.createElement("td");
-        var contParcela = document.createTextNode(i);
-    //var trAtual = document.getElementById("div1");
-        trNova.appendChild(contParcela);    
-    //document.body.insertBefore(trNova, trAtual);
-
-
-    $tr.appendChild(trNova)
-    i++
-    return $fragment.appendChild($tr);
-}  
-
- }
 }
