113673ad211c278fc80f77fec5fa97c7ecaed766
Ednei Vieira <ednei.gois167@gmail.com>
2021-02-03 23:39:04 -0400

    ajuste

--- js/fno.js
+++ js/fno.js
@@ -5,6 +5,7 @@ var parcela = parseInt(numeroDeParc.value)
 var valor =parseInt(valorfinanc.value)
 var valorparc = valor /parcela
 var i = 0
+var $tr = document.querySelector('[data-js="tr"]')
 
 button.onclick=  function(){
     for(var i=0; i < parseInt(numeroDeParc.value);i++ ){
@@ -15,18 +16,25 @@ button.onclick=  function(){
 }
  function criarpar(){
     //var $fragment = document.createDocumentFragment();
-    var $tr = document.createElement('tr');
+    //var $tr = document.createElement('tr');
 
-    var trNova = document.createElement("td");
-    var trNova1 = document.createElement("td");
-    var contParcela = document.createTextNode(i);
-    var contParcela = document.createTextNode(i);
-    var contParcela = document.createTextNode(i);
+    var trNova = document.createElement("td"); //node
+    var contParcela = document.createTextNode(i*i);
+
+    var trNova1 = document.createElement("td");//node
+    var contParcela1 = document.createTextNode(`tr  ${i}`);
+
+   // var contParcela = document.createTextNode(i+i+i+i);
+    //var contParcela = document.createTextNode(i);
+
+
     var trAtual = document.getElementById("div1");
+    var trAtual = document.getElementById("div21");
     
     //document.body.insertBefore(trNova, trAtual);
 
     trNova.appendChild(contParcela);
+    trNova1.appendChild(contParcela1);
     $tr.appendChild(trNova)
     $tr.appendChild(trNova1)
 
