6b56f103ba3d219a8adb152fb1245618d99c7fbc
Ednei Vieira <ednei.gois167@gmail.com>
2021-02-11 00:12:44 -0400

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,8 +1,10 @@
-var button = document.querySelector('[data-js="incluir"]')
-var valorfinanc = document.querySelector('[data-js="valorfinanc"]')
-var numeroDeParc = document.querySelector('[data-js="totalparc"]')
-var taxa = document.querySelector('[data-js="taxa"]')
-var parcela = parseInt(numeroDeParc.value)
-var valor =parseInt(valorfinanc.value)
-var valorparc = parseInt(valorfinanc.value) /parseInt(numeroDeParc.value)
-var i = 1
+// var button = document.querySelector('[data-js="incluir"]')
+// var valorfinanc = document.querySelector('[data-js="valorfinanc"]')
+// var numeroDeParc = document.querySelector('[data-js="totalparc"]')
+// var taxa = document.querySelector('[data-js="taxa"]')
+// var parcela = parseInt(numeroDeParc.value)
+// var valor =parseInt(valorfinanc.value)
+// var valorparc = parseInt(valorfinanc.value) /parseInt(numeroDeParc.value)
+// var i = 1
+
+//não usar
