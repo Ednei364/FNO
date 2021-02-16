9a532ebcfa8c20fb0069758c4898e45223eb5734
Ednei Vieira <ednei.gois167@gmail.com>
2021-02-15 21:55:42 -0400

    ajuste

--- js/fno.js
+++ js/fno.js
@@ -17,7 +17,20 @@ valorfinanc.onkeyup =  function limiteFinanc(){
     valor = valor + ''
     valor= parseInt(valor.replace(/\D+/g, ''))
     valor = valor + '';
+    valor = valor.replace(/([0-9]{2})$/g, ",$1");
+    
+    if (valor.length > 9) {
+        valor = valor.replace(/([0-9]{3})([0-9]{3}),([0-9]{2}$)/g, ".$1.$2,$3 ");
+    }
+    if (valor.length > 6) {
+        valor = valor.replace(/([0-9]{3}),([0-9]{2}$)/g, ".$1,$2");
+    }
+
+    valorfinanc.value = valor;
     if(valor == 'NaN') valorfinanc.value = '';
+
+    
+
 }
 numeroDeParc.onkeyup=  function limiteFinanc(){
     var valor = numeroDeParc.value;
@@ -38,13 +51,14 @@ taxa.onkeyup=   function limiteFinanc(){
 
 //gerar contrato
 
-button.onclick=  function(){   
-    var valorparc = parseFloat(valorfinanc.value) /parseFloat(numeroDeParc.value) 
+button.onclick=  function(){  
+    var fin1 = parseFloat(((valorfinanc.value).replace(/\./gi,'')).replace(/,/,'.')) 
+    var valorparc = fin1 /parseFloat(numeroDeParc.value) 
     var parcela = parseFloat(numeroDeParc.value)
     var taxa1 = parseFloat(taxa.value)
-    if( numeroDeParc.value>120){
+    if( numeroDeParc.value>420){
         erro.innerHTML="Numero de parcelas acima do permitido"
-    }else if(isNaN(valorparc)||valorparc<1 ){
+    }else if(isNaN(fin1)||fin1<0.001 ){
             erro.innerHTML="valor não informado"
         }else if(isNaN(parcela)||parcela<1){
             erro.innerHTML="Parcela não informada"
@@ -53,7 +67,9 @@ button.onclick=  function(){
         }else{        
         if(paras>0){
             console.log(`sss ${paras}`)
-            erro.innerHTML="Contrato já lançado, favor atualizar a página para nova simulação"
+            //alert("Existre uma simulação de Fno já lançado, a pagina será recarregada")
+            window.location.reload()
+            
         }else{
             for(var i=0; i < parseFloat(numeroDeParc.value);i++ ){
                 erro.innerHTML=""
@@ -66,11 +82,26 @@ button.onclick=  function(){
     }
     
 
+
  function criarpar(){
-    var Amort = parseFloat(valorfinanc.value) /parseFloat(numeroDeParc.value)
-    var saldo = (parseFloat(valorfinanc.value)-(Amort*i))
+    var fin = parseFloat(((valorfinanc.value).replace(/\./gi,'')).replace(/,/,'.'))
+    //console.log(fin)
+    var Amort = fin /parseFloat(numeroDeParc.value)
+    var saldo = fin+Amort-(Amort*i)
     var taxa1 =(saldo * parseFloat(taxa.value)/100)
     var parc =Amort+taxa1
+    var valor1 =function valor1(){
+        if(Amort>1){
+            return /([0-9]{3})([0-9]{3}).([0-9]{2}$)/g
+        } 
+    }   
+    var tt = Amort.length
+    var valor2=valor1()
+    console.log(`${valor2} sf +${i}`)
+    console.log(`${tt} `)
+    
+
+    
     
     //var $fragment = document.createDocumentFragment();
     var $tr = document.createElement('tr');
@@ -79,7 +110,7 @@ button.onclick=  function(){
     var contParcela = document.createTextNode(`${i}ª`);
 
     var trNova1 = document.createElement("td");//node
-    var contParcela1 = document.createTextNode(`R$  ${Amort.toFixed(2)}`);
+    var contParcela1 = document.createTextNode(`R$  ${Amort.toFixed(2).replace(valor2,'$1.$2,$3')}`);
 
     var trNova2 = document.createElement("td");//node
     var contParcela2 = document.createTextNode(`R$ ${taxa1.toFixed(2)}`);
@@ -117,22 +148,6 @@ button.onclick=  function(){
 
  }
  function criarpsar(){
-    var valor = valorfinanc.value;
-    valor = valor + ''
-    valor= parseInt(valor.replace(/\D+/g, ''))
-    valor = valor + '';
-    valor = valor.replace(/([0-9]{2})$/g, ",$1");
-    
-    if (valor.length > 9) {
-        valor = valor.replace(/([0-9]{3})([0-9]{3}),([0-9]{2}$)/g, ".$1.$2,$3 ");
-    }
-    if (valor.length > 6) {
-        valor = valor.replace(/([0-9]{3}),([0-9]{2}$)/g, ".$1,$2");
-    }
-
-    valorfinanc.value = valor;
-    if(valor == 'NaN') valorfinanc.value = '';
-
     taxa.value =`${taxa.value}%`
 }
   
